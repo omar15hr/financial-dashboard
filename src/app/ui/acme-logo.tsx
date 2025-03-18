d9ef9ef6e05d390f3a7829930d0457d6dacb3889
omar <omarhrn@gmail.com>
2025-03-18 10:17:46 -0300

    añadiendo fonts, images responsive, estructure of the main page

--- src/app/ui/acme-logo.tsx
+++ src/app/ui/acme-logo.tsx
@@ -1,4 +1,4 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { Globe } from 'lucide-react';
 import { lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
@@ -6,8 +6,8 @@ export default function AcmeLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <Globe className="h-12 w-12 rotate-[15deg]" />
+      <p className="text-[44px]">Financial Dashboard</p>
     </div>
   );
 }
